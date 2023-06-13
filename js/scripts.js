539e16ad865a1fa2c5807126b4dad053d2ab3c50
Brandon Wright <brandonfullstack@gmail.com>
2023-06-12 22:43:38 -0400

    Add voter info functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,25 @@ window.onload = function () {
   form.onsubmit = function (event) {
     event.preventDefault();
 
-    // we access two HTML elements, the drink menu and the message to underaged folks,
+    // we access two HTML elements, the voter info and the message to underaged folks,
     // and add the hidden class to those elements;
     // doing this clears results before displaying new ones, which
     // allows the user to submit the form again and again, and
     // see new results.
-    let drinkMenu = document.getElementById("drinks");
-    drinkMenu.setAttribute("class", "hidden");
-    let under21Message = document.getElementById("under-21");
-    under21Message.setAttribute("class", "hidden");
+    let voterInfo = document.getElementById("voter");
+    voterInfo.setAttribute("class", "hidden");
+    let under18Message = document.getElementById("non-voter");
+    under18Message.setAttribute("class", "hidden");
 
     // we gather the age value
     const age = parseInt(document.querySelector("input#age").value);
 
-    // we check if the age is greater than 21.
-    const ageBool = age > 21; // ageBool will be set to true
+    // we check if the age is greater than 18.
+    const ageBool = age >= 18; // ageBool will be set to true
     if (ageBool) {
-      drinkMenu.removeAttribute("class");
+      voterInfo.removeAttribute("class");
     } else {
-      under21Message.removeAttribute("class");
+      under18Message.removeAttribute("class");
     }
   };
 };
